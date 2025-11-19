5fe5e4a39f2cb3cef7a332c26b870a11885df1eb
leezrix-harsh <veezlow@gmail.com>
2025-11-19 12:12:55 +0700

    Add files via upload

---
+++ out/_next/static/chunks/53a55827c4b03b22.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/Home", [
+  "static/chunks/58ffdb8da4e9a04f.js",
+  "static/chunks/8aaa4d456707dda2.js",
+  "static/chunks/0a7f9cea5775fc41.js",
+  "static/chunks/turbopack-77d785fab2cc85ea.js"
+])
---
+++ out/_next/static/chunks/d4c98bcebd5023b8.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/5e95d43555606957.js",
+  "static/chunks/8aaa4d456707dda2.js",
+  "static/chunks/0a7f9cea5775fc41.js",
+  "static/chunks/turbopack-a7e8eae62a619e1d.js"
+])
